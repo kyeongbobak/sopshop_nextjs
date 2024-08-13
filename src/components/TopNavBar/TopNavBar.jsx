5348840ac93d9757ad814e685819e8ff0d0be0ba
kyeongbobak <kyeong0914@gmail.com>
2024-08-13 22:08:58 +0900

    style : TopNavBar에서 SideMenu 애니메이션 스타일 추가

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import StyledLink from "next/link";
 import Image from "next/image";
 import logoImage from "../../../public/img/Logo-SopShop.png";
 import menuBtnImage from "../../../public/img/menu_icon.png";
 import styles from "../TopNavBar/TopNavBar.module.css";
+import { useState } from "react";
 
 export default function TopNavBar() {
+  const [slideState, setSlideState] = useState(null);
+
   return (
     <div className={styles.wrapper}>
       <StyledLink href={"/"}>
@@ -15,11 +20,40 @@ export default function TopNavBar() {
           <StyledLink href={"/cart"}>Cart</StyledLink>
         </li>
         <li>
-          <button>
+          <button onClick={() => setSlideState("slideIn")}>
             <Image className={styles.menuBtnImage} src={menuBtnImage} alt="menuBtnIcon" priority />
           </button>
         </li>
       </ul>
+      {slideState && (
+        <>
+          <div className={`${styles.sideNavBarOverLay} ${styles[slideState]}`} onClick={() => setSlideState("slideOut")}></div>
+          <div className={`${styles.sideNavBar} ${styles[slideState]}`}>
+            <ul>
+              <li>
+                <button>
+                  <StyledLink href={""}>Login</StyledLink>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <StyledLink href={""}>Join</StyledLink>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <StyledLink href={""}>My</StyledLink>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <StyledLink href={""}>Order</StyledLink>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
