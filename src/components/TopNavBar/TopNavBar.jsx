b342fab26023c30f6f9e30368c1f1084ee73fea6
kyeongbobak <kyeong0914@gmail.com>
2024-08-30 19:51:54 +0900

    feat : userType에 따른 네비게이션 바 조건부 렌더링 구현, CSS 스타일 추가

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { isLogin, userToken } from "../../recoil/atoms";
+import { isLogin, userToken, userType } from "../../recoil/atoms";
 import { userLogout } from "../../api/LoginOut";
 import { useRouter } from "next/navigation";
 import StyledLink from "next/link";
 import Image from "next/image";
 import logoImage from "../../../public/img/Logo-SopShop.png";
 import menuBtnImage from "../../../public/img/menu_icon.png";
+import shoppingBagIcon from "../../../public/img/icon-shopping-bag.png";
 import styles from "./TopNavBar.module.css";
 
 export default function TopNavBar() {
@@ -18,6 +19,7 @@ export default function TopNavBar() {
 
   const setIsLogin = useSetRecoilState(isLogin);
   const token = useRecoilValue(userToken);
+  const userTypeState = useRecoilValue(userType);
   const router = useRouter();
 
   const logoutMutation = useMutation({
@@ -41,51 +43,80 @@ export default function TopNavBar() {
       <StyledLink href={"/"}>
         <Image className={styles.logoImage} src={logoImage} alt="Logo" priority />
       </StyledLink>
-      <ul className={styles.navBar}>
-        <li>
-          <StyledLink href={"/cart"}>Cart</StyledLink>
-        </li>
-        <li>
-          <button onClick={() => setSlideState("slideIn")}>
-            <Image className={styles.menuBtnImage} src={menuBtnImage} alt="menuBtnIcon" priority />
-          </button>
-        </li>
-      </ul>
-      {slideState && (
+
+      {userTypeState === "BUYER" ? (
         <>
-          <div className={`${styles.sideMenuOverLay} ${styles[slideState]}`} onClick={() => setSlideState("slideOut")}></div>
-          <div className={`${styles.sideMenu} ${styles[slideState]}`}>
-            <ul className={styles.sideMenuInner}>
-              <li className={styles.sideMenuItem}>
-                {isLoginState ? (
-                  <button>
-                    <StyledLink href={"/accountsetup/loginout"} onClick={() => handleOnLogout()}>
-                      Logout
-                    </StyledLink>
-                  </button>
-                ) : (
-                  <button>
-                    <StyledLink href={"/accountsetup/loginout"}>Login</StyledLink>
-                  </button>
-                )}
-              </li>
-              <li className={styles.sideMenuItem}>
-                <button>
-                  <StyledLink href={"/accountsetup/signup"}>Join</StyledLink>
+          <ul className={styles.navBar}>
+            <li>
+              <StyledLink href={"/cart"}>Cart</StyledLink>
+            </li>
+            <li>
+              <button onClick={() => setSlideState("slideIn")}>
+                <Image className={styles.menuBtnImage} src={menuBtnImage} alt="menuBtnIcon" priority />
+              </button>
+            </li>
+          </ul>
+          {slideState && (
+            <>
+              <div className={`${styles.sideMenuOverLay} ${styles[slideState]}`} onClick={() => setSlideState("slideOut")}></div>
+              <div className={`${styles.sideMenu} ${styles[slideState]}`}>
+                <ul className={styles.sideMenuInner}>
+                  <li className={styles.sideMenuItem}>
+                    {isLoginState ? (
+                      <button>
+                        <StyledLink href={"/accountsetup/loginout"} onClick={() => handleOnLogout()}>
+                          Logout
+                        </StyledLink>
+                      </button>
+                    ) : (
+                      <button>
+                        <StyledLink href={"/accountsetup/loginout"}>Login</StyledLink>
+                      </button>
+                    )}
+                  </li>
+                  <li className={styles.sideMenuItem}>
+                    <button>
+                      <StyledLink href={"/accountsetup/signup"}>Join</StyledLink>
+                    </button>
+                  </li>
+                  <li className={styles.sideMenuItem}>
+                    <button>
+                      <StyledLink href={""}>My Page</StyledLink>
+                    </button>
+                  </li>
+                  <li className={styles.sideMenuItem}>
+                    <button>
+                      <StyledLink href={""}>Order</StyledLink>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <ul className={styles.navBar}>
+            <li>
+              <button className={styles.sellerCenterNavigateBtn}>
+                <Image src={shoppingBagIcon} alt="shoppingBagIcon" width={24} />
+                <StyledLink href={"/"}>
+                  <span>판매자 센터</span>
+                </StyledLink>
+              </button>
+            </li>
+            <li className={styles.NavBarItem}>
+              {isLoginState ? (
+                <button className={styles.LoginOutNavigateBtn}>
+                  <StyledLink href={"/accountsetup/loginout"}>Logout</StyledLink>
                 </button>
-              </li>
-              <li className={styles.sideMenuItem}>
-                <button>
-                  <StyledLink href={""}>My Page</StyledLink>
+              ) : (
+                <button className={styles.LoginOutNavigateBtn}>
+                  <StyledLink href={"/accountsetup/loginout"}>Login</StyledLink>
                 </button>
-              </li>
-              <li className={styles.sideMenuItem}>
-                <button>
-                  <StyledLink href={""}>Order</StyledLink>
-                </button>
-              </li>
-            </ul>
-          </div>
+              )}
+            </li>
+          </ul>
         </>
       )}
     </div>
