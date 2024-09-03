b1790c08631fe58fca8fc77712c87ee837d00e96
kyeongbobak <kyeong0914@gmail.com>
2024-09-03 20:03:48 +0900

    feat : 계정 검증하는 API, 사업자 등록번호 검증하는 API 연동

--- src/api/Account.js
+++ src/api/Account.js
@@ -1,3 +1,5 @@
 import { apiPost } from "../lib/api/utils";
 
 export const validateAccount = (userId, token) => apiPost(`/accounts/signup/valid/username/`, userId, token);
+
+export const validateCompanyNumber = (body) => apiPost(`/accounts/signup/valid/company_registration_number/`, body);
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -2,7 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { validateAccount } from "../../api/Account";
+import { useMutation } from "@tanstack/react-query";
+import { validateAccount, validateCompanyNumber } from "../../api/Account";
 import { userToken } from "../../recoil/atoms";
 import { useRecoilValue } from "recoil";
 import Image from "next/image";
@@ -14,10 +15,11 @@ import styles from "./SignUpForm.module.css";
 export default function SignUpForm() {
   const [isBuyer, setIsBuyer] = useState(true);
   const [activeOption, setActiveOption] = useState(false);
-  const [warningMessage, setWarningMessage] = useState();
+
+  const token = useRecoilValue(userToken);
 
   const frontNumberList = ["010", "011", "016", "017", "018", "019"];
-  const token = useRecoilValue(userToken);
+
   const {
     register,
     watch,
@@ -32,6 +34,11 @@ export default function SignUpForm() {
   const userPassword = watch("password", "");
   const userPasswordConfirm = watch("passwordConfirm", "");
   const userName = watch("username", "");
+  const userFrontNumber = watch("frontNumber", "");
+  const userMiddleNumber = watch("middleNumber", "");
+  const userEndNumber = watch("endNumber", "");
+
+  const phoneNumber = [userFrontNumber, userMiddleNumber, userEndNumber].join("");
 
   // 비밀번호 유효성 검사
   useEffect(() => {
@@ -62,20 +69,60 @@ export default function SignUpForm() {
     }
   }, [setError, clearErrors, userPassword, userPasswordConfirm]);
 
-  const verifyUserName = async () => {
+  // 휴대폰 번호 유효성 검사
+  useEffect(() => {
+    if (userMiddleNumber || userEndNumber) {
+      if (!/^\d{11}$/.test(phoneNumber) & phoneNumber) {
+        setError("phoneNumber", {
+          type: "phoneNumber-maxlength",
+          message: "휴대폰 번호는 10자리 또는 11자리 숫자여야 합니다.",
+        });
+      } else {
+        clearErrors("");
+      }
+    }
+  }, [setError, clearErrors, phoneNumber, userMiddleNumber, userEndNumber]);
+
+  // 게정 검증하기
+  const validateAccountMutation = useMutation({
+    mutationFn: validateAccount,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const verifyUserName = () => {
     const body = {
       username: `${userName}`,
     };
-    await validateAccount(body, token);
+    validateAccountMutation.mutate(body, token);
+  };
+
+  // 사업자 등록번호 검증하기
+  const verifyCompanyNumberMutation = useMutation({
+    mutationFn: validateCompanyNumber,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const verifyCompanyNumber = () => {
+    const { companyNumber } = getValues();
+    const body = {
+      company_registration_number: `${companyNumber}`,
+    };
+
+    verifyCompanyNumberMutation.mutate(body);
   };
 
   const handleOnSignUp = (data) => {
     console.log(data);
-    const { frontNumber, middleNumber, endNumber } = getValues();
-
-    if (!frontNumber || !middleNumber || !endNumber) {
-      setWarningMessage("휴대폰 번호를 입력해주세요!");
-    }
   };
 
   return (
@@ -87,11 +134,11 @@ export default function SignUpForm() {
         </label>
         <div className={styles.userIdWrapper}>
           <input
+            className={`${styles.styledInput} ${styles.userIdInput}`}
             type="text"
             {...register("username", {
               required: "아이디를 입력해주세요.",
             })}
-            className={`${styles.styledInput} ${styles.userIdInput}`}
           />
           <button className={styles.actionBtn} type="button" onClick={verifyUserName}>
             중복확인
@@ -112,14 +159,14 @@ export default function SignUpForm() {
         <label className={styles.styledLabel} htmlFor="">
           비밀번호 재확인
         </label>
-        <input type="password" {...register("passwordConfirm")} className={styles.styledInput} />
+        <input className={styles.styledInput} type="password" {...register("passwordConfirm")} />
         {errors.passwordConfirm && <p className={styles.errorMessage}>{errors.passwordConfirm.message}</p>}
         <label className={styles.styledLabel} htmlFor="">
           이름
         </label>
         <input
-          type="text"
           className={styles.styledInput}
+          type="text"
           {...register("name", {
             required: "이름을 입력해주세요.",
           })}
@@ -130,7 +177,7 @@ export default function SignUpForm() {
         </label>
         <div className={styles.phoneNumberWrapper}>
           <div>
-            <input {...register("frontNumber")} defaultValue={"010"} type="text" className={`${styles.styledInput} ${styles.phoneNumberInput} ${styles.frontNumberInput}`} />
+            <input className={`${styles.styledInput} ${styles.phoneNumberInput} ${styles.frontNumberInput}`} type="text" {...register("frontNumber")} defaultValue={"010"} />
             <button className={styles.selectBtn} onClick={() => setActiveOption(!activeOption)}>
               {activeOption ? <Image width={18} src={downArrow} alt="downArrowIcon" priority /> : <Image width={18} src={upArrow} alt="upArrowIcon" priority />}
             </button>
@@ -150,25 +197,52 @@ export default function SignUpForm() {
               </ul>
             )}
           </div>
-          <input type="text" className={`${styles.styledInput} ${styles.phoneNumberInput}`} {...register("middleNumber")} />
-          <input type="text" className={`${styles.styledInput} ${styles.phoneNumberInput}`} {...register("endNumber")} />
+          <input
+            className={`${styles.styledInput} ${styles.phoneNumberInput}`}
+            type="text"
+            {...register("middleNumber", {
+              required: "휴대폰 번호를 입력해주세요",
+            })}
+          />
+          <input
+            className={`${styles.styledInput} ${styles.phoneNumberInput}`}
+            type="text"
+            {...register("endNumber", {
+              required: "휴대폰 번호를 입력해주세요.",
+            })}
+          />
         </div>
-        <p className={styles.errorMessage}>{warningMessage}</p>
+        {(errors.middleNumber || errors.endNumber) && <p className={styles.errorMessage}>{errors.middleNumber?.message || errors.endNumber?.message}</p>}
+        {errors.phoneNumber && <p className={styles.errorMessage}>{errors.phoneNumber.message}</p>}
         {!isBuyer && (
           <div className={styles.sellerSignUpForm}>
             <label className={styles.styledLabel} htmlFor="">
               사업자 등록번호
             </label>
             <div className={styles.userIdWrapper}>
-              <input type="text" className={`${styles.styledInput} ${styles.userIdInput}`} />
-              <button className={styles.actionBtn} type="button">
+              <input
+                className={`${styles.styledInput} ${styles.userIdInput}`}
+                type="text"
+                {...register("companyNumber", {
+                  required: "사업자 등록번호를 추가해주세요.",
+                })}
+              />
+              <button className={styles.actionBtn} type="button" onClick={verifyCompanyNumber}>
                 인증
               </button>
             </div>
+            {errors.companyNumber && <p className={styles.errorMessage}>{errors.companyNumber.message}</p>}
             <label className={styles.styledLabel} htmlFor="">
               스토어 이름
             </label>
-            <input type="text" className={styles.styledInput} />
+            <input
+              className={styles.styledInput}
+              type="text"
+              {...register("storeName", {
+                required: "스토어명을 입력해주세요.",
+              })}
+            />
+            {errors.storeName && <p className={styles.errorMessage}>{errors.storeName.message}</p>}
           </div>
         )}
         <div className={styles.otherSectionWrapper}>
@@ -177,7 +251,7 @@ export default function SignUpForm() {
             sopshop의 <span>이용약관</span> 및 <span>개인 정보 처리 방침</span>에 대한 내용을 확인하였고 동의합니다.
           </p>
         </div>
-        <button type="submit" className={styles.signBtn}>
+        <button className={styles.signBtn} type="submit">
           SIGN IN
         </button>
       </form>
