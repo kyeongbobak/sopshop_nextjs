373d74a082f2e6502d1814f611fb0da63b599a6a
kyeongbobak <kyeong0914@gmail.com>
2024-09-19 16:36:16 +0900

    style : 404 Not Found 파일 생성, 레이아웃 구현, CSS 스타일 추가

---
+++ src/app/not-found.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import styles from "../styles/not-found.module.css";
+
+export const metadata = {
+  title: "Not Found",
+};
+
+export default function notFound(props) {
+  console.log(props);
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <strong className={styles.errorMessage}>페이지를 찾을 수 없습니다.</strong>
+        <div className={styles.errorDescriptionWrapper}>
+          <p className={styles.errorDescription}>페이지가 존재하지 않거나 사용할 수 없는 페이지입니다.</p>
+          <p className={styles.errorDescription}>웹 주소가 올바른지 확인해주세요.</p>
+        </div>
+        <div>
+          <Link className={styles.navigateBtnWrapper} href="/">
+            <button className={styles.navigateBtn}>메인으로</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
