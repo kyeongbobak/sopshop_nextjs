99730c18ffbe6a685237035c92d4966e6fc50f98
kyeongbobak <kyeong0914@gmail.com>
2024-10-08 20:45:04 +0900

    style : favicon 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="./Ellipse 1.png" />
+      </head>
       <body>
         <QueryClientProvider client={queryClient}>
           <RecoilRoot>{children}</RecoilRoot>
